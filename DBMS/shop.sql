bdb9a7d8c9028442b6f29db2b820c969e99b78ec
Muhammed Shamil KP <130832663+muhammedshamil8@users.noreply.github.com>
2025-06-28 17:32:11 +0530

    Update shop.sql

--- DBMS/shop.sql
+++ DBMS/shop.sql
@@ -1,5 +1,5 @@
 create table shop(id number(4),name varchar(20),price number(10),quantity number(10));
-table created
+
 insert into shop values(1,'Book',25,30);
 insert into shop values(2,'Pen',10,20);
 insert into shop values(3,'Chart',10,10);
@@ -10,7 +10,7 @@ create or replace procedure sale(i in number, q in number) is
 begin
 update shop set quantity =q where id=i;
 end;
- procedure created
+
 declare 
 i number(4):=:Enter_Item_Id;
 q number(10):=:Enter_new_quantity;
