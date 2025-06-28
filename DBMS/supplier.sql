66dadae877de162348f69edc1bc6584c3a4b9d29
Muhammed Shamil KP <130832663+muhammedshamil8@users.noreply.github.com>
2025-06-28 17:30:54 +0530

    Update supplier.sql

--- DBMS/supplier.sql
+++ DBMS/supplier.sql
@@ -1,53 +1,69 @@
-create table supplier (supcode char(3) primary key ,sname varchar(10) , City varchar(10));
+CREATE TABLE supplier (supcode CHAR(3) PRIMARY KEY, sname VARCHAR(10), city VARCHAR(10));
+CREATE TABLE product (pcode CHAR(3) PRIMARY KEY, pname VARCHAR(20));
+CREATE TABLE supl_product (supcode CHAR(3) REFERENCES supplier(supcode), pcode CHAR(3) REFERENCES product(pcode), qty INT);
 
-create table product (pcode char(3) references supplier (supcode), pcode char(3) references product (pcode), oty INT );
+INSERT INTO supplier VALUES 
+('s1', 'Raju', 'calicut'), 
+('s2', 'sheela', 'thissur'),
+('s3', 'Aysha', 'kochi'),
+('s4', 'Anees', 'Tirur'),
+('s5', 'Lisa', 'calicut'),
+('s6', 'zara', 'tirur');
 
-create table supl_product(supcode references supplier(supcode),pcode references product(pcode), qty eferences product(oty))
+SELECT * FROM supplier;
 
-INSERT INTO supplier values (‘s1’’Raju’,’calicut’), (‘s2’,’sheela,’’thissur’),
-(‘s3’,’Aysha’,’kochi’),(‘s4’,’Anees’,’Tirur’),(‘s5’,’Lisa’,’calicut’),(‘s6’,’zara’,’tirur’);
+INSERT INTO product VALUES 
+('p1', 'Soap'), 
+('p2', 'Rice'), 
+('p3', 'Salt'), 
+('p4', 'Sugar'), 
+('p5', 'Wheat'), 
+('p6', 'Colgate'), 
+('p7', 'Chilly');
 
-select * from supplier;
+SELECT * FROM product;
 
-INSERT INTO Product VALUES ('p1', 'Soap'), ('p2', 'Rice'), ('p3', 'Salt'), ('p4', 'Sugar'), ('p5', 'Wheat'), ('p6', 'Colgate'), ('p7', 'Chilly');
+INSERT INTO supl_product VALUES 
+('s1', 'p1', 34), 
+('s1', 'p2', 20), 
+('s2', 'p4', 5), 
+('s3', 'p3', 10), 
+('s2', 'p7', 10), 
+('s3', 'p1', 12);
 
-SELECT * FROM Product;
-
-INSERT INTO Supl_Product VALUES ('s1', 'p1', 34), ('s1', 'p2', 20), ('s2', 'p4', 5), ('s3', 'p3', 10), ('s2', 'p7', 10), ('s3', 'p1', 12);
-
-SELECT * FROM Supl_Product;
+SELECT * FROM supl_product;
 
 -- a)
-SELECT a.Supcode, b.Supcode, b.City 
-FROM Supplier a, Supplier b 
-WHERE a.City = b.City AND a.Supcode < b.Supcode;
+SELECT a.supcode, b.supcode, b.city 
+FROM supplier a, supplier b 
+WHERE a.city = b.city AND a.supcode < b.supcode;
 
 -- b)
-SELECT Sname 
-FROM Supplier 
-WHERE Supcode IN (SELECT Supcode FROM Supl_Product WHERE pcode = 'p2');
+SELECT sname 
+FROM supplier 
+WHERE supcode IN (SELECT supcode FROM supl_product WHERE pcode = 'p2');
 
 -- c)
 SELECT pcode 
-FROM Supl_Product 
+FROM supl_product 
 GROUP BY pcode 
 HAVING COUNT(pcode) > 1;
 
 -- d)
-SELECT Supcode 
-FROM Supplier 
-WHERE City = (SELECT City FROM Supplier WHERE Supcode = 's1');
+SELECT supcode 
+FROM supplier 
+WHERE city = (SELECT city FROM supplier WHERE supcode = 's1');
 
 -- e)
-SELECT Sname 
-FROM Supplier 
-WHERE Supcode IN (SELECT Supcode FROM Supl_Product WHERE pcode = 'p1');
+SELECT sname 
+FROM supplier 
+WHERE supcode IN (SELECT supcode FROM supl_product WHERE pcode = 'p1');
 
 -- f)
-SELECT COUNT(DISTINCT Supcode) 
-FROM Supl_Product;
+SELECT COUNT(DISTINCT supcode) 
+FROM supl_product;
 
 -- g)
-SELECT pcode, SUM(Qty) 
-FROM Supl_Product 
+SELECT pcode, SUM(qty) 
+FROM supl_product 
 GROUP BY pcode;
